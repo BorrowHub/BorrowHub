69a3c19e671433fbe4125d61c56f73aa53a1e855
rommeljhon <jhonrommelcloma63@gmail.com>
2025-12-11 12:59:05 +0800

    borrow_item

--- src/Finals/Dashboard.java
+++ src/Finals/Dashboard.java
@@ -1,5 +1,23 @@
 
 package Finals;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBConnection {
+    public static Connection getConnection() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/borrowhub",
+                "root",
+                ""
+            );
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
 
 public class Dashboard extends javax.swing.JFrame {
 
